603c5237419f0f4747e84f6c6a1cad6dca721062
scott <scott@tams.tech>
2018-02-03 02:22:34 -0500

    bugfixes

--- alpha/index.jsx
+++ alpha/index.jsx
@@ -8,35 +8,35 @@ const dom = ReactDOM;
 
 const DocumentRoot = ()=>{
   return (
-    <Header />
-    <div id="intro" className="panel">{IntroText}</div>
-    {sections.map(
-      function sect(section) {
-        if section.text.typeof === "string" {
-          return <Pane
-            identifier={section.identifier}
-            title={section.title}>{section.text}</div>
-        }
-        else {
-          if Array.isArray(section.text.typeof) {
-            section.map(function subsect(subsection){
-              return sect(subsection);
-            });
-          }
-          else{
-            console.log(dedent`
-              ERROR: Malformed child of ${section.title} follows:
-                ${section.text}
-              Type: ${typeof(section.text)}
+    <body>
+      <Header />
+      <div id="intro" className="panel">{IntroText}</div>
+      {sections.map(
+        function sect(section) {
+          if( section.text.typeof === "string" ){
+            return <Pane
+              identifier={section.identifier}
+              title={section.title}>{section.text}</div>
+          } else {
+            if Array.isArray(section.text.typeof) {
+              section.map(function subsect(subsection){
+                return sect(subsection);
+              });
+            } else{
+              console.log(dedent`
+                ERROR: Malformed child of ${section.title} follows:
+                  ${section.text}
+                Type: ${typeof(section.text)}
 
-              Children of sections should be either strings/JSX or an array
-              of more sections.`
-            );
+                Children of sections should be either strings/JSX or an array
+                of more sections.`
+              );
+            }
           }
         }
-      }
-    )}
-    <Footer links=FooterLinks />
+      )}
+      <Footer links=FooterLinks />
+    </body>
   )
 }
 
