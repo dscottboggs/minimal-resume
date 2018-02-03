8382a77f779dfc031b855dbfd45277805995b0f4
scott <scott@tams.tech>
2018-02-03 00:21:10 -0500

    Rendered the document root and linked it to index.html

--- alpha/index.jsx
+++ alpha/index.jsx
@@ -2,46 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './content';
 import Header, Pane, Footer from './components';
-import dedent from 'dedent-js';
 
-dom = ReactDOM
+const dedent = require('dedent-js');
+const dom = ReactDOM;
 
 const DocumentRoot = ()=>{
   return (
-    <head>
-      <meta charset="utf-8" />
-      <link href="stylesheet.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-      <Header />
-      <div id="intro" className="panel">{IntroText}</div>
-      {sections.map(
-        function sect(section) {
-          if section.text.typeof === "string" {
-            return <Pane
-              identifier={section.identifier}
-              title={section.title}>{section.text}</div>
+    <Header />
+    <div id="intro" className="panel">{IntroText}</div>
+    {sections.map(
+      function sect(section) {
+        if section.text.typeof === "string" {
+          return <Pane
+            identifier={section.identifier}
+            title={section.title}>{section.text}</div>
+        }
+        else {
+          if Array.isArray(section.text.typeof) {
+            section.map(function subsect(subsection){
+              return sect(subsection);
+            });
           }
-          else {
-            if Array.isArray(section.text.typeof) {
-              section.map(function subsect(subsection){
-                return sect(subsection);
-              });
-            }
-            else{
-              console.log(dedent`
-                ERROR: Malformed child of ${section.title} follows:
-                  ${section.text}
-                Type: ${typeof(section.text)}
+          else{
+            console.log(dedent`
+              ERROR: Malformed child of ${section.title} follows:
+                ${section.text}
+              Type: ${typeof(section.text)}
 
-                Children of sections should be either strings/JSX or an array
-                of more sections.`
-              );
-            }
+              Children of sections should be either strings/JSX or an array
+              of more sections.`
+            );
           }
         }
-      )}
-      <Footer links=FooterLinks />
-    </body>
+      }
+    )}
+    <Footer links=FooterLinks />
   )
 }
+
+dom.render(<DocumentRoot />, dom.getElementById('document') );
