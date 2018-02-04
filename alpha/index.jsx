06a2b2eab1a1cab8e7a500c58f3c4e240d3fdd15
Scott <scott@MasterSkuzz.local>
2018-02-04 00:48:39 -0500

    I do not understand why that was a function as opposed to a class.
    Also, I cleaned it up some.

--- alpha/index.jsx
+++ alpha/index.jsx
@@ -6,37 +6,41 @@ import { Header, Pane, Footer } from './components';
 const dedent = require('dedent-js');
 const dom = ReactDOM;
 
-const DocumentRoot = ()=>{
-  return (
-    <div id="bodywrapper">
-      <Header />
-      <div id="intro" className="panel">{IntroText}</div>
-      {sections.map(
-        function sect(section) {
-          if( section.text.typeof === "string" ){
-            const pane = <Pane
-              identifier={section.identifier}
-              title={section.title}>{section.text}></Pane>;
-            return pane;
-          } else if (Array.isArray(section.text.typeof)) {
-              section.map(function subsect(subsection){
-                return sect(subsection);
-              })
-          } else {
-              console.log( dedent`
-                ERROR: Malformed child of ${section.title} follows:
-                  ${section.text}
-                Type: ${typeof(section.text)}
+class DocumentRoot extends React.Component {
+  sect(section) {
+    if( section.text.typeof === "string" ){
+      const pane = <Pane
+        identifier={section.identifier}
+        title={section.title}>{section.text}></Pane>;
+      return pane;
+    } else if (Array.isArray(section.text.typeof)) {
+        section.map(function subsect(subsection){
+          return sect(subsection);
+        })
+    } else {
+        console.log( dedent`
+          ERROR: Malformed child of ${section.title} follows:
+            ${section.text}
+          Type: ${typeof(section.text)}
 
-                Children of sections should be either strings/JSX or an array
-                of more sections.` )
-              return <div id="errordiv"></div>
-          }
-        }
-      )}
-      <Footer links={FooterLinks} />
-    </div>
-  )
+          Children of sections should be either strings/JSX or an array
+          of more sections.` )
+        return <div id="errordiv"></div>
+    }
+  }
+  render() {
+    return (
+      <div id="bodywrapper">
+        <Header />
+        <div id="intro" className="panel">{IntroText}</div>
+        {this.props.sections.map(this.sect(section))}
+        <Footer links={this.props.footerlinks} />
+      </div>
+    )
+  }
 }
 
-dom.render(<DocumentRoot />, document.getElementById('document') );
+dom.render(
+  <DocumentRoot sections=Sections footerlinks=FooterLinks />,
+  document.getElementById('document')
+);
