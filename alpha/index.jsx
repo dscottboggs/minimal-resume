5334ba771614e48639c299d3a27e7d0f327a141f
D. Scott Boggs <scott@tams.tech>
2018-02-08 00:04:48 -0500

    Fixes to calls to DocumentRoot.sect()

--- alpha/index.jsx
+++ alpha/index.jsx
@@ -6,12 +6,16 @@ import { Header, PaneParent, Footer } from './components';
 const dedent = require('dedent-js');
 
 class DocumentRoot extends React.Component {
+    constructor(props) {
+        super(props);
+        this.sect = this.sect.bind(this);
+    }
     sect(section) {
         return (<PaneParent
             identifier={section.identifier}
             title={section.title}
             children={section.text}></PaneParent>
-        )
+        );
     }
     render() {
         return (
@@ -19,10 +23,10 @@ class DocumentRoot extends React.Component {
                 <Header />  //The header
                 <div id="intro" className="panel">{IntroText}</div>
                 // ^^ the text at the top that doesn't really fit in a section
-                {this.props.sections.map(this.sect(section))}  // a loop over all of the sections
+                {this.props.sections.map((section) => this.sect(section))}  // a loop over all of the sections
                 <Footer links={this.props.footerlinks} />    // the quick links section at the bottom
             </div>
-        )
+        );
     }
 }
 
