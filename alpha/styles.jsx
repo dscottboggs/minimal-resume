096de805b010ff128893efca448819c12e291629
D. Scott Boggs <scott@tams.tech>
2018-02-11 11:45:43 -0500

    Moved most styles to styles.jsx.

---
+++ alpha/styles.jsx
@@ -0,0 +1,71 @@
+
+const introStyle = {
+    display: "inline";
+    width: "60%";
+    text-align: "right";
+}
+
+const panelHeaderStyle = {
+    position: "relative",
+    padding: 5,
+    text-align: "right",
+    border: "none",
+    font-size: "2em",
+    font-weight: "bold",
+    font-family: "Worksans,sans-serif"
+}
+const panelStyle = {
+    font-size: "1.15em";
+    padding: 5;
+    display: "none";
+    text-align: "left";
+    font-weight: "normal";
+}
+const headerTextStyle = {
+    text-align: "left";
+    font-weight: "normal";
+    padding-left: "15%";
+    font-family: "Worksans,sans-serif";
+}
+const footerBlockStyle = {
+    display: "inline";
+    width: "100%";
+    border-top: "1.5em solid #FED";
+    border-bottom: "1.5em solid #FED";
+    text-align: "center";
+    font-size: "1em";
+    font-family: "Quicksand, sans-serif";
+    position: "fixed";
+    bottom: 0;
+    background: "#FED";
+}
+
+const footerTitleStyle = {
+    font-size: "1.15em";
+    font-weight: "bold";
+    margin-top: "-0.5em";
+    margin-bottom: "-0.5em";
+    font-family: "WorkSans, sans-serif";
+}
+
+const footerLinkStyle = {
+    margin-left: "2em";
+    margin-right: "2em";
+    font-family: "Quicksand, sans-serif";
+    display: "inline";
+    z-index: 1000;
+    font-size: "1em";
+    font-weight: "normal";
+}
+
+const documentRootStyle = {
+    background:#FED;
+    color:#013;
+}
+const mainBodyStyle = {
+    position:"absolute";
+    font-family: "Quicksand,sans-serif";
+    margin-left: "15%";
+    padding-bottom: "5em";
+    width: "65%";
+}
