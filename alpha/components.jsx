72e7f5d1ce06c673e1e4cd97debb038e3d9e924c
scott <scott@tams.tech>
2018-03-16 14:54:36 -0400

    no, I didn't

--- alpha/components.jsx
+++ alpha/components.jsx
@@ -20,10 +20,10 @@ const bodyFontSize = 1.15;
 const animationTime = '0.5s';
 const buttonPadding = 5;
 const footerFontFamily = "Quicksand, sans-serif";
-const backgroundColor = "#013";
-const secondaryBackgroundColor = "#FED";
-const foregroundColor = "#FED";
-const secondaryForegroundColor = "#013";
+const backgroundColor = "#FED";
+const secondaryBackgroundColor = "#013";
+const foregroundColor = "#013";
+const secondaryForegroundColor = "#FED";
 function getColor(selected, theme){
     if (selected===true) {
         if (theme.toLowerCase()==="fg") {
