3239f754ca01a14d8283ac12ed2c872ac0666ba5
D. Scott Boggs <scott@tams.tech>
2018-10-19 01:05:55 -0400

    empty bash variables need quotes

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-cd `[ -n $PROJECT_DIR ] && printf $PROJECT_DIR || printf /home/scott/Documents/code/web/minimal_resume/`
+cd `[ -n "$PROJECT_DIR" ] && printf "$PROJECT_DIR" || printf /media/deployments/minimal-resume/`
 git pull
 docker build --pull \
              --quiet \
-             --tag minimal_resume \
-             /home/scott/Documents/code/minimal_resume/
+             --tag minimal-resume \
+             /media/deployments/minimal-resume/
 cd -
